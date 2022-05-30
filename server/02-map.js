8602932f52e1cd6bad62e2188b6159ca3111ff34
Misael-GC <misael.gomez1623@gmail.com>
2022-05-30 15:30:25 -0500

    map()

---
+++ server/02-map.js
@@ -0,0 +1,16 @@
+const letters = ['a', 'b', 'c'];
+
+//map nos ahorramos todas las lineas del for, map genera un nuevo array y hace los cambios ahi
+const newArray = letters.map(item => item + '++');
+
+//con for: generamos una transformacion que se haga en un new array
+// const newArray =[];
+// for (let i = 0; i < letters.length; i++){
+//     const element = letters[i];
+//     newArray.push(element + '++'); //el ++ es la modificacion
+// }
+console.log('original', letters);
+console.log('new', newArray);
+
+// result: original [ 'a', 'b', 'c' ]
+//         new [ 'a++', 'b++', 'c++' ]
