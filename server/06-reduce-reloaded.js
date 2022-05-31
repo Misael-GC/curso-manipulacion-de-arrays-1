89d6b73626cfe9f02dc14a8071791e2ffa3fd8a3
Misael-GC <misael.gomez1623@gmail.com>
2022-05-31 12:19:30 -0500

    reto: acumular de rangos y no individual

--- server/06-reduce-reloaded.js
+++ server/06-reduce-reloaded.js
@@ -42,7 +42,7 @@ const data = [
     },
   ];
 
-  //aqui vamos a concatenar map y reduce, asi se hace 
+  //aqui vamos a concatenar map y reduce, asi se hace, se puede agregar filtro y si hubiera por fecha hacerlo
   const rta1 = data
   .map(item => item.level)
   .reduce((obj, item) => {
@@ -57,3 +57,25 @@ const data = [
 console.log(rta1);
 
 //resultado : { low: 2, medium: 2, hight: 2 }
+
+
+
+//reto: contar por rangos, de forma agrupada, 1-5, 6-8, 9-10
+
+const numbers = [3, 10, 3, 2, 3,10,10,10,10,10,10, 6,6,,7,8,5,3,10];
+
+//estructura del calculo principal de rango
+const beerween = (x, min, max) => x >= min && x <= max;
+//.reduce para que sume cada item que se repita
+const rangoRepetitions = numbers.reduce((acumulador, item) => {
+    //aplicando  la estructura del calculo principal
+    if(beerween (item, 1,5)) acumulador["1-5"] +=1;// si 3 esta entre 1-5 suma 1
+    if  (beerween (item, 6,8)) acumulador["6-8"] +=1;//si  7 esta entre 6-8 suma 1
+    if (beerween (item, 9,10)) acumulador["9-10"] +=1; //si  10 esta entre 9-10 suma 1
+
+    return acumulador;
+},{"1-5": 0, "6-8": 0, "9-10": 0,}) //estado inicial del objeto
+  /////////////////////------------------
+console.log(rangoRepetitions);
+
+// result :  { '1-5': 6, '6-8': 4, '9-10': 8 }
