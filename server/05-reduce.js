121fb2015b8bf3c7ee1c49378049768396a1accb
Misael-GC <misael.gomez1623@gmail.com>
2022-05-30 22:46:29 -0500

    reduce a un resultado final, suma final

---
+++ server/05-reduce.js
@@ -0,0 +1,14 @@
+const totals = [1,2,3,4,5];
+
+//como lo haciamos antes
+let sum = 0;
+for (let i = 0; i < totals.length; i++){
+    const element = totals[i];
+    sum = sum + element;
+}
+console.log(sum);
+
+//reduce, con menos lineas de code
+//sum = acumulador, | element = indice [i]
+const rta = totals.reduce((sum, element) => sum + element, 0)// (parametro1, parametro2) => parametro1, parametro2 | el 0 es el parametro2 y es el estado inicial
+console.log('rta', rta);
