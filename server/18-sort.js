68b53cc346c8960426dfcc1b5aa756bd223f1262
Misael-GC <misael.gomez1623@gmail.com>
2022-06-01 17:09:29 -0500

    fecha y hora -Aporte

--- server/18-sort.js
+++ server/18-sort.js
@@ -173,3 +173,33 @@ const ordersA = [
   ];
   ordersA.sort((a,b) => new Date(b.datesA) - new Date(a.datesA));
   console.log('Aporte', ordersA);
+
+  //aporte con fecha y horas
+  const ordersWDate = [
+    {
+        customerName: "Nicolas",
+        total: 600,
+        delivered: true,
+        date: new Date(2022, 1, 14, 15, 45)
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+        date: new Date(2021, 11, 18, 18)
+    },
+    {
+        customerName: "Santiago",
+        total: 1840,
+        delivered: true,
+        date: new Date(2021, 11, 24, 20, 30)
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+        date: new Date(2022, 0, 18, 14, 15)
+    },
+];
+ordersWDate.sort((a, b) => b.date - a.date)
+console.log(ordersWDate)
